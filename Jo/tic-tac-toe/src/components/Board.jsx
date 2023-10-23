f9ce383db80e718e4f30e307e547f3fa834a5c6f
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-10-22 20:36:31 -0700

    Project initiated
    Board displayed & styling placed

---
+++ Jo/tic-tac-toe/src/components/Board.jsx
@@ -0,0 +1,24 @@
+import Strike from "./Strike"
+import Tile from "./Tile"
+
+const Board = () => {
+  return (
+    <div className="board">
+        <Tile />
+        <Tile />
+        <Tile />
+
+        <Tile />
+        <Tile />
+        <Tile />
+
+        <Tile />
+        <Tile />
+        <Tile />
+
+        <Strike />
+    </div>
+  )
+}
+
+export default Board
---
+++ Jo/tic-tac-toe/src/components/Tile.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Tile = () => {
+  return (
+    <div className='tile'>X</div>
+  )
+}
+
+export default Tile
